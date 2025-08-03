1086d947f6e50bdcd4d6afb5860a076e30254b11
Juan Carlos Cuascota <78097538+mdcrys@users.noreply.github.com>
2025-08-03 14:58:14 -0500

    actulizar admin

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -156,12 +156,10 @@ export const environment = {
       'thumbnail': './assets/media/demos/demo20.png'
     },
   },
-  URL_BACKEND: 'http://api-doc.sisgesdoc.com/',
-  URL_SERVICIOS: 'http://api-doc.sisgesdoc.com/api',
-  URL_FRONTED: 'http://doc.sisgesdoc.com',
- /* URL_BACKEND: 'https://api-doc.sisgesdoc.com/',
+  
+  URL_BACKEND: 'https://api-doc.sisgesdoc.com/',
 URL_SERVICIOS: 'https://api-doc.sisgesdoc.com/api',
-URL_FRONTED: 'https://doc.sisgesdoc.com',*/
+URL_FRONTED: 'https://doc.sisgesdoc.com',
 
 };
 
